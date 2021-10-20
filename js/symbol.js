681a61610c5423b90b2fa1aa1e90c73417920081
Jack <warmrain0513@hotmail.com>
2021-10-20 23:59:30 +0800

    Rename and Code調整

--- js/symbol.js
+++ js/symbol.js
@@ -1,133 +1,76 @@
-;(function() {
-  let symbolName = ['', '乾', '兌', '離', '震', '巽', '坎', '艮', '坤']
-  let elements = ['木', '火', '土', '金', '水']
-  let symbolElement = [-1, 3, 3, 1, 0, 0, 4, 2, 2]
-  let data = {
-    first: 0,
-    second: 0,
-    symbols: [0, 0, 0, 0, 0, 0, 0],
-    useTime: ''
+const symbolName = ['', '乾', '兌', '離', '震', '巽', '坎', '艮', '坤']
+const elements = ['木', '火', '土', '金', '水']
+const symbolElement = [-1, 3, 3, 1, 0, 0, 4, 2, 2]
+
+const validateInput = function(input) {
+  return !/^\d+$/.test(input) || input === 0
+}
+
+// 拆解成三個符號
+//[symbolTop,symbolMed,symbolBottom]
+// 0 陽 1 陰
+const disassembleSymbols = function(symbol) {
+  const symbolIndex = symbol - 1
+  const symbolTop = symbolIndex % 2
+  const symbolMed = symbolIndex % 4 > 1 ? 1 : 0
+  const symbolBottom = symbolIndex > 3 ? 1 : 0
+  return [symbolTop, symbolMed, symbolBottom]
+}
+
+// 組合三個符號計算
+const calculateSymbolNumber = function(top, med, bottom) {
+  return bottom * 4 + med * 2 + top + 1
+}
+
+// 計算變換的位置
+const getChangeSymbolIndex = function(no1, no2, hour) {
+  return (parseInt(no1) + parseInt(no2) + Math.floor(((hour + 1) % 24) / 2)) % 6
+}
+
+// 進行爻變 1.拆解 2.變化 3.組合
+const getChangeSymbol = function(symbol, changeIndex) {
+  changeIndex = 2 - (changeIndex % 3)
+  let symbols = disassembleSymbols(symbol)
+  symbols[changeIndex] = (symbols[changeIndex] + 1) % 2
+  return calculateSymbolNumber(symbols[0], symbols[1], symbols[2])
+}
+
+// 計算吉凶
+const getElementsResult = function(baseElement, compElement) {
+  const result = ['大吉', '大凶', '小吉', '小凶', '大吉']
+  const resultIndex = (baseElement - compElement + 4) % 5
+  return result[resultIndex]
+}
+
+// ================================================================
+// 將三個值計算結果
+const getSymbolResult = function(first, second, hour) {
+  const changeSymbolIndex = getChangeSymbolIndex(first, second, hour)
+  const noChange = changeSymbolIndex < 3
+
+  const firstValue = ((first - 1) % 8) + 1
+  const secondValue = ((second - 1) % 8) + 1
+  const baseValue = noChange ? firstValue : secondValue
+  const oneValue = noChange ? secondValue : firstValue
+  // 將掛拆解重新組合
+  const firstArray = disassembleSymbols(firstValue)
+  const secondArray = disassembleSymbols(secondValue)
+  const twoValue = calculateSymbolNumber(
+    firstArray[2],
+    secondArray[0],
+    secondArray[1]
+  )
+  const threeValue = calculateSymbolNumber(
+    firstArray[1],
+    firstArray[2],
+    secondArray[0]
+  )
+  let fourValue = getChangeSymbol(oneValue, changeSymbolIndex)
+
+  return {
+    firstValue,
+    secondValue,
+    baseValue,
+    value: [oneValue, twoValue, threeValue, fourValue]
   }
-
-  let validateInput = function(input) {
-    return !/^\d+$/.test(input) || input === 0
-  }
-
-  //[symbolTop,symbolMed,symbolBottom]
-  // 0 陽 1 陰
-  let calculateSymbol = function(symbol) {
-    let symbolIndex = symbol - 1
-    let symbolTop = symbolIndex % 2
-    let symbolMed = symbolIndex % 4 > 1 ? 1 : 0
-    let symbolBottom = symbolIndex > 3 ? 1 : 0
-    return [symbolTop, symbolMed, symbolBottom]
-  }
-
-  let calculateSymbolNumber = function(top, med, bottom) {
-    return bottom * 4 + med * 2 + top + 1
-  }
-
-  let getChangeSymbolIndex = function(no1, no2, hour) {
-    let total =
-      parseInt(no1) + parseInt(no2) + Math.floor(((hour + 1) % 24) / 2)
-    return total % 6
-  }
-
-  let getChangeSymbol = function(symbol, changeIndex) {
-    changeIndex = 2 - (changeIndex % 3)
-    let symbols = calculateSymbol(symbol)
-    symbols[changeIndex] = (symbols[changeIndex] + 1) % 2
-    symbol = calculateSymbolNumber(symbols[0], symbols[1], symbols[2])
-    return symbol
-  }
-
-  let getElementsResult = function(baseElement, compElement) {
-    let result = ['大吉', '大凶', '小吉', '小凶', '大吉']
-    let resultIndex =
-      (symbolElement[baseElement] - symbolElement[compElement] + 4) % 5
-    return result[resultIndex]
-  }
-
-  // ================================================================
-
-  let getSymbolResult = function(first, second, hour) {
-    let firstValue = ((first - 1) % 8) + 1
-    let secondValue = ((second - 1) % 8) + 1
-    let changeSymbolIndex = getChangeSymbolIndex(first, second, hour)
-    console.log(first, second, hour, changeSymbolIndex)
-
-    let noChange = changeSymbolIndex < 3
-    let baseValue = noChange ? firstValue : secondValue
-    let oneValue = noChange ? secondValue : firstValue
-    let firstArray = calculateSymbol(firstValue)
-    let secondArray = calculateSymbol(secondValue)
-    let twoValue = calculateSymbolNumber(
-      firstArray[2],
-      secondArray[0],
-      secondArray[1]
-    )
-    let threeValue = calculateSymbolNumber(
-      firstArray[1],
-      firstArray[2],
-      secondArray[0]
-    )
-    let fourValue = getChangeSymbol(oneValue, changeSymbolIndex)
-    return {
-      value: [
-        firstValue,
-        secondValue,
-        baseValue,
-        oneValue,
-        twoValue,
-        threeValue,
-        fourValue
-      ]
-    }
-  }
-
-  // ================================================================
-  new Vue({
-    el: '#app',
-    data: data,
-
-    methods: {
-      imageSrc: function(i) {
-        return this.changeImageSrc(this.symbols[i])
-      },
-      changeImageSrc: function(value) {
-        return `./image/symbol${value}.png`
-      },
-      getWord: function(i) {
-        let word = ''
-        let symbol = this.symbols[i]
-        if (symbol !== 0) {
-          word = symbolName[symbol] + elements[symbolElement[symbol]]
-        }
-        return word
-      },
-      elementsResult: function(i) {
-        let result = ''
-        let symbol = this.symbols[i]
-        if (symbol !== 0) {
-          result = getElementsResult(this.symbols[2], symbol)
-        }
-        return result
-      },
-      calculation: function() {
-        if (validateInput(this.first) || validateInput(this.second)) {
-          alert('請輸入正數')
-          return
-        }
-        let date = new Date()
-        let hour = date.getHours()
-        let result = getSymbolResult(this.first, this.second, hour)
-        if (result) {
-          this.useTime = `計算時間:${date.getHours()}點${date.getMinutes()}分`
-          this.symbols = result.value
-        } else {
-          alert('計算失敗，請重新計算')
-        }
-      }
-    }
-  })
-})()
+}
---
+++ js/symbolVue.js
@@ -0,0 +1,57 @@
+;(function() {
+  let data = {
+    first: 0,
+    second: 0,
+    firstValue: 0,
+    secondValue: 0,
+    baseValue: 0,
+    symbols: [0, 0, 0, 0],
+    useTime: ''
+  }
+  // ================================================================
+  new Vue({
+    el: '#app',
+    data: data,
+
+    methods: {
+      imageSrc(value) {
+        return `./image/symbol${value}.png`
+      },
+      getWord(symbol) {
+        let word = ''
+        if (symbol !== 0) {
+          word = symbolName[symbol] + elements[symbolElement[symbol]]
+        }
+        return word
+      },
+      elementsResult(symbol) {
+        let result = ''
+        if (symbol !== 0) {
+          result = getElementsResult(
+            symbolElement[this.baseValue],
+            symbolElement[symbol]
+          )
+        }
+        return result
+      },
+      calculation() {
+        if (validateInput(this.first) || validateInput(this.second)) {
+          alert('請輸入正數')
+          return
+        }
+        let date = new Date()
+        let hour = date.getHours()
+        let result = getSymbolResult(this.first, this.second, hour)
+        if (result) {
+          this.useTime = `計算時間:${date.getHours()}點${date.getMinutes()}分`
+          this.firstValue = result.firstValue
+          this.secondValue = result.secondValue
+          this.baseValue = result.baseValue
+          this.symbols = result.value
+        } else {
+          alert('計算失敗，請重新計算')
+        }
+      }
+    }
+  })
+})()
