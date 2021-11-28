acbd9f0b801258bfda0faf8d33da8d856a79d170
Jack <warmrain0513@hotmail.com>
2021-11-28 11:19:33 +0800

    增加自選時間

---
+++ js/symbolVue3.js
@@ -0,0 +1,154 @@
+;(function() {
+  let data = {
+    first: 0,
+    second: 0,
+    nowTime: new Date(),
+    timer: null,
+    timeIndex: -1,
+    chooseTime: 0,
+    selectTime: 0,
+    selectMin: 0
+  }
+  // ================================================================
+  new Vue({
+    el: '#app',
+    data: data,
+    mounted() {
+      this.timer = setInterval(() => (this.nowTime = new Date()), 1000)
+    },
+    beforeDestroy() {
+      clearInterval(this.timer)
+    },
+    computed: {
+      chooseUseTime() {
+        if (this.chooseTime === 0) {
+          return {
+            hours: this.nowTime.getHours(),
+            min: this.nowTime.getMinutes()
+          }
+        } else {
+          return {
+            hours: this.selectTime,
+            min: this.selectMin
+          }
+        }
+      },
+      useTime() {
+        this.checkTimeChange
+        return `計算時間:${this.chooseUseTime.hours}點${this.chooseUseTime.min}分`
+      },
+      canChange() {
+        return (
+          this.first !== '' &&
+          this.second !== '' &&
+          this.first !== 0 &&
+          this.second !== 0
+        )
+      },
+      firstValue() {
+        let result = 0
+        if (this.canChange) {
+          result = this.changeInput(this.first)
+        }
+        return result
+      },
+      secondValue() {
+        let result = 0
+        if (this.canChange) {
+          result = this.changeInput(this.second)
+        }
+        return result
+      },
+      hourIndex() {
+        return Math.floor(((this.chooseUseTime.hours + 1) % 24) / 2)
+      },
+      checkTimeChange() {
+        if (this.firstValue !== 0 && this.secondValue !== 0) {
+          const hourIndex = this.hourIndex
+          if (hourIndex !== this.timeIndex) {
+            this.timeIndex = hourIndex
+          }
+        }
+      },
+      changeSymbolIndex() {
+        let result = -1
+        if (this.timeIndex > -1) {
+          result = getChangeSymbolIndex(this.first, this.second, this.timeIndex)
+        }
+        return result
+      },
+      checkNoChange() {
+        let noChange = true
+        if (this.changeSymbolIndex > -1) {
+          noChange = this.changeSymbolIndex < 3
+        }
+        return noChange
+      },
+      baseValue() {
+        let result = 0
+        if (
+          this.changeSymbolIndex > -1 &&
+          this.firstValue > 0 &&
+          this.secondValue > 0
+        ) {
+          result = this.checkNoChange ? this.firstValue : this.secondValue
+        }
+        return result
+      },
+      symbols() {
+        let result = [0, 0, 0, 0]
+        if (this.baseValue > 0) {
+          const one = this.checkNoChange ? this.secondValue : this.firstValue
+          const firstArray = disassembleSymbols(this.firstValue)
+          const secondArray = disassembleSymbols(this.secondValue)
+          const two = calculateSymbolNumber(
+            firstArray[2],
+            secondArray[0],
+            secondArray[1]
+          )
+          const three = calculateSymbolNumber(
+            firstArray[1],
+            firstArray[2],
+            secondArray[0]
+          )
+          let four = getChangeSymbol(one, this.changeSymbolIndex)
+          result = [one, two, three, four]
+        }
+        return result
+      }
+    },
+    methods: {
+      changeInput(inputValue) {
+        let result = 0
+        if (validateInput(inputValue)) {
+          alert('請輸入正數')
+        } else {
+          result = ((inputValue - 1) % 8) + 1
+        }
+        return result
+      },
+      imageSrc(value) {
+        return `./image/symbol${value}.png`
+      },
+      getWord(symbol) {
+        let word = ''
+        if (symbol !== 0) {
+          word = symbolName[symbol] + elements[symbolElement[symbol]]
+        }
+        return word
+      },
+      elementsResult(symbol) {
+        let result = ''
+        if (symbol !== 0) {
+          result = getElementsResult(
+            this.baseValue,
+            symbol,
+            this.firstValue,
+            this.secondValue
+          )
+        }
+        return result
+      }
+    }
+  })
+})()
